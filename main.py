fbaac34e2c17bbf86f8efb3d6486df7f8e22e612
Johannes Johansson <johannes.johansson@queenslab.se>
2023-02-11 18:11:24 +0100

    deploy-ready

--- main.py
+++ main.py
@@ -3,91 +3,51 @@ Created on 2021-07-07 17:14
 @author: johannes
 """
 import flet as ft
-# from settings import *
-from shoe import *
-from utils import *
+from shoe import DataHandler
+from utils import get_base64
 
-APPBAR_TITLES = {
-    "home": ft.Text(""),
-    "statistic": ft.Text("Shoe stats"),
+
+TABLE_COLUMNS = {
+    "Name": "name",
+    "Runs": "number_of_runs",
+    "Km": "accumulated_distance_km",
+    "Added": "time_of_creation"
 }
-
-
-def get_new_shoe_data():
-    return {
-        "time_of_creation": get_time_now(),
-        "accumulated_distance_km": 0,
-        "number_of_runs": 0
-    }
-
-
-class DataHandler:
-    def __init__(self, page: ft.Page):
-        self.page = page
-
-    def add_shoe(self, name: str):
-        shoes = self.page.client_storage.get('shoes') or {}
-        shoes.setdefault(name, get_new_shoe_data())
-        self.page.client_storage.set('shoes', shoes)
-        self.set_selected_shoe(name)
-
-    def add_distance_to_shoe(self, name, distance=None):
-        try:
-            distance = float(distance.replace(',', '.'))
-        except:
-            return
-        shoes = self.page.client_storage.get('shoes')
-        shoes[name]["accumulated_distance_km"] += distance
-        shoes[name]["number_of_runs"] += 1
-        self.page.client_storage.set('shoes', shoes)
-
-    def get_shoe(self, name: str):
-        shoes = self.page.client_storage.get('shoes') or {}
-        return shoes.get(name) or {}
-
-    def get_shoe_list(self):
-        shoes = self.page.client_storage.get('shoes')
-        return list(shoes if shoes else [])
-
-    def get_selected_shoe(self):
-        return self.page.client_storage.get('selected') or ""
-
-    def set_selected_shoe(self, name):
-        self.page.client_storage.set('selected', name)
 
 
 def main(page: ft.Page):
     page.title = "Shoetracker"
     data_handler = DataHandler(page)
 
-    class AppBarSpecified(ft.AppBar):
-        def __init__(self, title: str = None):
-            super().__init__(
-                leading_width=40,
-                title=ft.Text(title),
-                actions=[
-                    ft.IconButton(ft.icons.BAR_CHART_SHARP, on_click=lambda _: layout_change("statistic")),
-                    ft.IconButton(ft.icons.HOME_SHARP, on_click=lambda _: layout_change("home"))
-                ]
-            )
+    def get_distance_dialog_title():
+        return ft.Text(f"Add distance to {data_handler.get_selected_shoe()}")
+
+    def get_table_rows():
+        def get_cell_content(attr, shoe):
+            if attr == "name":
+                return ft.Row(
+                    [ft.Icon(name="check_circle", color="green"), ft.Text(shoe.get(attr))],
+                    alignment=ft.MainAxisAlignment.START
+                )
+            else:
+                return ft.Text(shoe.get(attr))
+        shoes = data_handler.get_all_shoes()
+        return [
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(get_cell_content(attr, shoe))
+                    for attr in TABLE_COLUMNS.values()
+                ],
+            ) for shoe in shoes.values()
+        ]
 
     def layout_change(page_name):
-        page.appbar.title = APPBAR_TITLES.get(page_name)
         page.controls = []
         if page_name == "home":
-            add_item(home_content)
+            add_item(home_page)
         elif page_name == "statistic":
-            ...
-            # add_item(text_field, list_view)
-
-        if page_name != "statistic":
-            ...
-        page.update()
-
-    def remove_item(*items):
-        for item in items:
-            if item in page.controls:
-                page.controls.remove(item)
+            data_table.rows = get_table_rows()
+            add_item(stats_page)
         page.update()
 
     def add_item(*items):
@@ -117,20 +77,22 @@ def main(page: ft.Page):
         page.update()
 
     def open_distance_dlg_modal(e):
+        distance_dlg_modal.title = get_distance_dialog_title()
         page.dialog = distance_dlg_modal
         distance_dlg_modal.open = True
         page.update()
 
     def add_distance_dlg(e):
         if distance_text.value:
-            data_handler.add_shoe(new_shoe_text.value)
+            data_handler.add_distance_to_shoe(
+                shoes_dropdown.value, distance=distance_text.value)
             distance_text.value = ""
-        dlg_modal.open = False
+        distance_dlg_modal.open = False
         page.update()
 
     def on_dropdown_change(e):
         data_handler.set_selected_shoe(shoes_dropdown.value)
-        distance_dlg_modal.title = ft.Text(f"Add distance to {shoes_dropdown.value}")
+        distance_dlg_modal.title = ft.Text(f"Add distance to {shoes_dropdown.value or ''}")
 
     new_shoe_text = ft.TextField(helper_text="Example: Sense Ride 4")
     dlg_modal = ft.AlertDialog(
@@ -145,13 +107,16 @@ def main(page: ft.Page):
     )
     shoes_dropdown = ft.Dropdown(options=[ft.dropdown.Option(_shoe) for _shoe in data_handler.get_shoe_list()],
                                  value=data_handler.get_selected_shoe(),
-                                 on_change=on_dropdown_change)
+                                 on_change=on_dropdown_change,
+                                 filled=True,
+                                 bgcolor="white"
+                                 )
     add_shoe_button = ft.ElevatedButton("Add new shoe", on_click=open_dlg_modal)
 
     distance_text = ft.TextField(helper_text="Km - Example: 17.3")
     distance_dlg_modal = ft.AlertDialog(
         modal=True,
-        title=ft.Text(f"Add distance to {data_handler.get_selected_shoe()}"),
+        title=get_distance_dialog_title(),
         content=distance_text,
         actions=[
             ft.TextButton("Add", on_click=add_distance_dlg),
@@ -165,31 +130,54 @@ def main(page: ft.Page):
         icon_color="green400",
         on_click=open_distance_dlg_modal
     )
-
-    home_content = ft.Column(
+    app_bar = ft.Row(
         [
-            ft.Container(
-                content=ft.Column(
-                    [
-                        ft.Container(
-                            content=ft.Row(
-                                [shoes_dropdown, add_shoe_button],
-                                alignment=ft.MainAxisAlignment.CENTER,
+            ft.IconButton(ft.icons.BAR_CHART_SHARP, on_click=lambda _: layout_change("statistic")),
+            ft.IconButton(ft.icons.HOME_SHARP, on_click=lambda _: layout_change("home"))
+        ],
+        alignment=ft.MainAxisAlignment.END,
+    )
+    home_page = ft.Container(
+        image_src_base64=get_base64("assets/jogging.png"),
+        image_fit=ft.ImageFit.COVER,
+        expand=True,
+        content=ft.Column(
+            [
+                app_bar,
+                ft.Container(
+                    content=ft.Column(
+                        [
+                            ft.Container(
+                                content=ft.Row(
+                                    [shoes_dropdown, add_shoe_button],
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                ),
+                                alignment=ft.alignment.center,
                             ),
-                            alignment=ft.alignment.center,
-                        ),
-                        ft.Container(
-                            content=add_distance_button,
-                            alignment=ft.alignment.center
-                        )
-                    ], alignment=ft.MainAxisAlignment.SPACE_EVENLY
-                ), height=400
+                            ft.Container(
+                                content=add_distance_button,
+                                alignment=ft.alignment.center
+                            )
+                        ], alignment=ft.MainAxisAlignment.SPACE_EVENLY
+                    ), height=400
+                )
+            ]
+        )
+    )
+    data_table = ft.Row(
+        [
+            ft.DataTable(
+                columns=[ft.DataColumn(ft.Text(col)) for col in TABLE_COLUMNS],
+                rows=get_table_rows()
             )
-        ]
+        ], alignment=ft.MainAxisAlignment.CENTER
+    )
+    stats_page = ft.Container(
+        content=ft.Column([app_bar, data_table]),
+        alignment=ft.alignment.top_center
     )
 
-    page.appbar = AppBarSpecified()
-    add_item(home_content)
+    add_item(home_page)
 
 
 ft.app(
@@ -198,163 +186,3 @@ ft.app(
     assets_dir="assets",
     view=ft.WEB_BROWSER
 )
-
-# class MainApp(MDApp):
-#     """
-#     """
-#     data_table = None
-#
-#     def __init__(self, **kwargs):
-#         super(MainApp, self).__init__(**kwargs)
-#         Settings()
-#
-#         self.shoes = ShoeStore()
-#         if not self.shoes.shoe_list:
-#             self.change_setting('selected_shoe', "No shoe selected")
-#
-#     def on_start(self):
-#         self.setup_data_table()
-#
-#     def show_add_shoe_dialog(self, *args):
-#         dialog = MDDialog(
-#             title="Add shoe:",
-#             type="custom",
-#             content_cls=DialogContentAddShoe(),
-#             buttons=[
-#                 MDFlatButton(
-#                     text="Cancel",
-#                     text_color=self.theme_cls.primary_color,
-#                     on_release=lambda x: dialog.dismiss()
-#                 ),
-#                 MDFlatButton(
-#                     text="Add",
-#                     text_color=self.theme_cls.primary_color,
-#                     on_release=lambda x: (self.shoes.add_shoe(name=dialog.content_cls.ids.shoe_name.text),
-#                                           self._change_shoe_button(dialog.content_cls.ids.shoe_name.text),
-#                                           dialog.dismiss(), self.update_data_table())
-#                 ),
-#             ],
-#         )
-#         dialog.open()
-#
-#     def show_add_distance_dialog(self):
-#         dialog = None
-#         if not self.get_setting('selected_shoe') or self.get_setting('selected_shoe') == "No shoe selected":
-#             dialog = MDDialog(
-#                 text="No shoes, please add one :)",
-#                 buttons=[
-#                     MDFlatButton(
-#                         text="Cancel",
-#                         text_color=self.theme_cls.primary_color,
-#                         on_release=lambda x: dialog.dismiss()
-#                     ),
-#                     MDFlatButton(
-#                         text="Add shoe",
-#                         text_color=self.theme_cls.primary_color,
-#                         on_release=lambda x: (dialog.dismiss(), self.show_add_shoe_dialog())
-#                     ),
-#                 ],
-#             )
-#         else:
-#             dialog = MDDialog(
-#                 title="Add distance to {}".format(self.get_setting('selected_shoe')),
-#                 type="custom",
-#                 content_cls=DialogContentAddDistance(),
-#                 buttons=[
-#                     MDFlatButton(
-#                         text="Cancel",
-#                         text_color=self.theme_cls.primary_color,
-#                         on_release=lambda x: dialog.dismiss()
-#                     ),
-#                     MDFlatButton(
-#                         text="Add",
-#                         text_color=self.theme_cls.primary_color,
-#                         on_release=lambda x: (self.shoes.add_run(
-#                             name=self.get_setting('selected_shoe'),
-#                             distance=dialog.content_cls.ids.distance_text.text,
-#                             km=True if self.get_setting('selected_length_unit') == "km" else False,
-#                             miles=True if self.get_setting('selected_length_unit') == "miles" else False,
-#                         ), dialog.dismiss(), self.update_data_table())
-#                     ),
-#                 ],
-#             )
-#         dialog.open()
-#
-#     def open_shoe_menu(self):
-#         menu = None
-#         menu_list = [{
-#             "text": "Add new shoe",
-#             "viewclass": "IconListItem",
-#             "icon": "plus-circle-outline",
-#             "on_release": lambda x=True: (self.show_add_shoe_dialog(x), menu.dismiss())}]
-#         menu_list = menu_list + [{
-#                 "text": shoe_name,
-#                 "viewclass": "IconListItem",
-#                 "icon": "shoe-sneaker",
-#                 "on_release": lambda x=shoe_name: (self._change_shoe_button(x), menu.dismiss()),
-#             } for shoe_name in self.shoes.shoe_list]
-#         menu = MDDropdownMenu(
-#             caller=self.root.ids.screen_manager.mainwin.ids.shoe_drop_item,
-#             items=menu_list,
-#             width_mult=4,
-#         )
-#         menu.bind()
-#         menu.open()
-#
-#     def setup_data_table(self):
-#         """
-#         ("[size=10]Shoe Name[/size]", dp(30)),
-#         """
-#         self.data_table = MDDataTable(
-#             size_hint=(1, .93),
-#             use_pagination=False,
-#             rows_num=20,
-#             check=False,
-#             column_data=[
-#                 ("", dp(1)),
-#                 ("Name", dp(30)),
-#                 ("Runs", dp(10)),
-#                 ("Km", dp(12)),
-#                 # ("Miles", dp(12)),
-#                 ("Added", dp(20)),
-#                 # ("Status", dp(10))
-#             ],
-#             row_data=self.shoes.table_data,
-#             sorted_on="Name",
-#             # sorted_order="ASC",
-#             elevation=2
-#         )
-#         self.data_table.bind(on_row_press=self.on_row_press)
-#         self.data_table.bind(on_check_press=self.on_check_press)
-#
-#         self.root.ids.screen_manager.statswin.ids.stats_table.add_widget(self.data_table)
-#
-#     def update_data_table(self):
-#         self.data_table.row_data = self.shoes.table_data
-#
-#     def _change_shoe_button(self, name):
-#         self.root.ids.screen_manager.mainwin.ids.shoe_drop_item.text = name
-#         self.change_setting('selected_shoe', name)
-#
-#     def switch_to_screen(self, screen_name):
-#         self.root.ids.screen_manager.current = screen_name
-#
-#     def on_row_press(self, instance_table, instance_row):
-#         print(instance_table, instance_row)
-#
-#     def on_check_press(self, instance_table, current_row):
-#         print(instance_table, current_row)
-#
-#     @staticmethod
-#     def get_setting(key):
-#         return Settings.get_setting(key)
-#
-#     @staticmethod
-#     def change_setting(key, value):
-#         return Settings.save_setting(key, value)
-#
-#     def _print(self, *args):
-#         print(args)
-#
-#
-# MainApp().run()
